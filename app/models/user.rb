a587c1a950f78a231e02d4c9c8deb8bd8e99afe6
Santiago Cartasegna <cartasegnals@gmail.com>
2022-02-16 09:51:46 -0300

    added user/mail validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimun: 3, maximum: 25 }
+    VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { VALID_EMAIL_REGEX }
 end
