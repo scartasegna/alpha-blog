ecee97c280c54a4a466318f9012b47ae235a5dcc
Santiago Cartasegna <cartasegnals@gmail.com>
2022-02-16 10:08:49 -0300

    added 1-m user-article + harcoded user on create/update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+    has_many :articles
+
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimun: 3, maximum: 25 }
     VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/i
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { VALID_EMAIL_REGEX }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
