722392fc1f21172bc63cb697df77156ff02df07a
Santiago Cartasegna <cartasegnals@gmail.com>
2022-02-23 09:20:38 -0300

    added edit template and updated action. Refactor set_user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
 
-    def new
-        @user = User.new
-    end
+  def new
+      @user = User.new
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:notice] = "User #{@user.username} Created. Welcome to Alpha Blog!!"
-            redirect_to articles_path
-       else
-            render 'new'
-       end
+  def edit
+    
+  end
+
+  def update
+    if (@user.update(user_params))
+      flash[:notice] = "Profile updated!!"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
 
-    private
-  
-    def set_user
-      @user = User.find(params[:id])
-    end
-  
-  
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def create
+      @user = User.new(user_params)
+      if @user.save
+          flash[:notice] = "User #{@user.username} Created. Welcome to Alpha Blog!!"
+          redirect_to articles_path
+      else
+          render 'new'
+      end
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 
 end
