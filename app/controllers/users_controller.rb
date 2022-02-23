98f7d0e364469271722ebbe144c1ca73b60432df
Santiago Cartasegna <cartasegnals@gmail.com>
2022-02-23 09:48:13 -0300

    added show profile for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
+
+  def show
+    @articles = @user.articles
+  end
+
+
   def new
       @user = User.new
   end
