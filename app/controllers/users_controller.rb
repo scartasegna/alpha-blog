6f5ce1ec74bbea1cc11bdeeb17eee63a7e6cb070
Santiago Cartasegna <cartasegnals@gmail.com>
2022-03-17 09:32:51 -0300

    added alert/notice css +  fixed user login bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,19 +30,19 @@ class UsersController < ApplicationController
 
 
   def create
-      @user = User.new(user_params)
-      if @user.save
-          session[:user_id] = @user.id
-          flash[:notice] = "User #{@user.username} Created. Welcome to Alpha Blog!!"
-          redirect_to articles_path
-      else
-          render 'new'
-      end
+    @user = User.new(user_params)
+    if @user.save
+        session[:user_id] = @user.id
+        flash[:notice] = "User #{@user.username} Created. Welcome to Alpha Blog!!"
+        redirect_to articles_path
+    else
+        render 'new'
+    end
   end
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if current_user == @user 
     flash[:notice] = "Account and all articles deleted"
     redirect_to articles_path
   end
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit your profile"
       redirect_to @user
     end
