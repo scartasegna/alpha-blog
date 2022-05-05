4900e76aac033ebe8bd51a1435f61985e9012f1a
Santiago Cartasegna <cartasegnals@gmail.com>
2022-05-05 09:46:02 -0300

    category update/edit back and front end implementation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,10 +22,26 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
 
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+       @category = Category.find(params[:id])
+       if (@category.update(category_params))
+           flash[:notce] = "Category name updated"
+           redirect_to @category
+       else
+        render 'edit'
+       end
+    end
     private
 
+    private
     def category_params
         params.require(:category).permit(:name)
     end
